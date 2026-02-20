ed115e75b6b2b5d4ddfade038cfc6cbefb25cb12
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2026-02-20 15:06:07 +0100

    Added some initial small preprocessing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Depression estimation using audio with machine learning models
 
 ## Dataset
-Use dataset_fetch.py script to fetch dataset. In the end, it will be around 5-6GB.
+Use dataset_fetch.py script to fetch dataset. Will need to kaggle auth. In the end, it will be around 5-6GB.
 
 ## THIS README WILL BE UPDATED AND FINISHED ONCE THE PROJECT IS COMPLETED
--- common.py
+++ common.py
@@ -1,3 +1,5 @@
+import librosa
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -31,3 +33,30 @@ def split_dataset_80_10_10(df, seed=42):
     train_df.to_csv("dataset/train.csv", index=False)
     val_df.to_csv("dataset/val.csv", index=False)
     test_df.to_csv("dataset/test.csv", index=False)
+
+# TODO: Remove interviewer (she is less loud or just use the transcript files)?
+# TODO: Split into windows of 5-10s?
+# TODO: Play with these preprocessing params once the pipeline is established
+def preprocess(file_path):
+    # 1. Load + resample + mono
+    audio, sr = librosa.load(file_path, sr=16000, mono=True)
+
+    print("Original length:", len(audio)/sr, "seconds")
+
+    # 2. Remove silence
+    audio_trimmed, _ = librosa.effects.trim(
+        audio,
+        top_db=20
+    )
+
+    print("After trimming:", len(audio_trimmed)/sr, "seconds")
+
+    # 3. Normalize
+    audio_normalized = audio_trimmed / np.max(np.abs(audio_trimmed))
+
+    return audio_normalized
+
+# TODO: Add extracting of MFCC
+
+if __name__ == "__main__":
+    preprocess("dataset/300_AUDIO.wav")
